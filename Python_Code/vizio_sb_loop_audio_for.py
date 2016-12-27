a161ae9c36c245e87831acb8f371c2b0f1fe90bd
at-robert <atttrobert@gmail.com>
2016-12-27 10:37:55 +0000

    [import] - To add Python Loop test program for 3251/2820

---
+++ Python_Code/vizio_sb_loop_audio_for.py
@@ -0,0 +1,44 @@
+import re
+import os
+import sys
+import shutil
+import chardet
+import time
+import RPi.GPIO as GPIO
+
+#----------------------------------------------------------------------
+def vizio_SB_2820_Loop():
+    count = 0
+    print "Start vizio sound bar 2820 Power On/Off loop test"
+
+    while 1:
+        time.sleep(1)
+        os.system('irsend SEND_ONCE VIZIO_TV KEY_RIGHT')
+        count = count + 1
+        print "LOOP COUNT = %d" %(count)
+
+#----------------------------------------------------------------------
+def vizio_SB_3251_Loop():
+    count = 0
+    print "Start vizio sound bar 3251 Power On/Off loop test"
+
+    while 1:
+        time.sleep(1)
+        os.system('irsend SEND_ONCE VIZIO_TV KEY_RIGHT')
+        count = count + 1
+        print "LOOP COUNT = %d" %(count)
+
+
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print 'no argument; Usage: vizio_sb_loop.py 3251'
+        sys.exit()
+
+    if(sys.argv[1] == '3251'):
+        vizio_SB_3251_Loop()
+    else:
+        vizio_SB_2820_Loop()
+    
+
