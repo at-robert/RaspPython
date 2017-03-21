5c0a02e3813dc7fc86e339f5f3986fbf4a638abe
robert.lo <robert.lo@amtran.com.tw>
2017-03-21 12:00:09 +0000

    [import] - To add RaspX IP get through MAC address and power off it remotely

--- Python_Code/raspx_ip.sh
+++ Python_Code/raspx_ip.sh
@@ -3,14 +3,4 @@ HiMediaMacIP=$(echo "$password\n" | sudo -S nmap -sP 192.168.0.* | awk '/Nmap sc
 HiMediaMacIP=$(echo "$HiMediaMacIP" | sed 's/=>//g' | sed 's/B8:27:EB:E0:38:F9//g' | sed 's/^[ \t]*//g' | sed 's/[ \t]*$//g')
 echo "RaspX IP = $HiMediaMacIP"
 
-sudo ssh pi@$HiMediaMacIP "sudo shutdown -h now"
-
-echo "Stoping HiMedia !!!"
-irsend SEND_ONCE HiMEDIA KEY_POWER
-sleep 3
-irsend SEND_ONCE DB0204 KEY_POWER
-sleep 3
-irsend SEND_ONCE VIZIO_TV KEY_POWER
-sleep 3
-echo "Hello world by Robert end!!!"
-sudo shutdown -h now
+sudo ssh pi@$HiMediaMacIP "ls -la"
