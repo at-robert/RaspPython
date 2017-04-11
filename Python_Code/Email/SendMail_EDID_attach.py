85224ef62f65513b76ad1a5e9b3279cf63805da1
at-robert <atttrobert@gmail.com>
2017-04-11 03:18:05 +0000

    [bug fix] - To fix build error for HDMI I2C EDID bin file Email sending function

--- Python_Code/Email/SendMail_EDID_attach.py
+++ Python_Code/Email/SendMail_EDID_attach.py
@@ -23,7 +23,7 @@ def sendEmail_attach(subject, fullpath):
         msg['From'] = sender
         msg['Reply-to'] = sender
         user = str('ee3amt@gmail.com').strip()                     # 取得使用者名稱
-        pw = str('fw_oem12').strip()                       # 取得密碼
+        pw = str('fw_oem13').strip()                       # 取得密碼
 
         msg.preamble = 'Multipart massage.\n'
 
@@ -53,27 +53,8 @@ if __name__ == "__main__":
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
-    if len(sys.argv) < 3:
-        # print 'no argument; Usage: SendMail_attach.py TITLE FILE'
-        # sys.exit()
-        print "Default Title and Path:"
-        subject = "FW5 Weekly Report"
-        fullpath = "/Volumes/robert.lo/Data/DOC/Weekly Report/FW5-report-wk%d.docx" %(datetime.datetime.now().isocalendar()[1]+1)
-        fullpath_next = "/Volumes/robert.lo/Data/DOC/Weekly Report/FW5-report-wk%d.docx" %(datetime.datetime.now().isocalendar()[1]+2)
-        # fullpath = "/Volumes/Jet_drive/FW5-report-wk40.docx"
-    else:
-        subject = str(sys.argv[1])
-        fullpath = str(sys.argv[2])
-
-    print "Title = %s" %(subject)
-    print "Full path = %s" %(fullpath)
-    print "The current Week Number is : W%d" %(datetime.datetime.now().isocalendar()[1])
-
-    if os.path.isfile(fullpath_next):
-        print "%s exist so the report was sent before!!" %(fullpath_next)
-        sys.exit()
-
-    fullpath = ~/edid.bin
+    subject = "HDMI I2C EDID table"
+    fullpath = "/home/pi/edid.bin"
 
     if not os.path.isfile(fullpath):
         print "%s doesn't exist" %(fullpath)
