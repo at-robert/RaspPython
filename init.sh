d07402ef7f09d1d8949e5ccaf88e30340566dc51
robert.lo <atttrobert@gmail.com>
2017-05-08 16:50:28 +0000

    [import] - To modify some init setting steps for Raspberry pi project

---
+++ init.sh
@@ -0,0 +1,21 @@
+sudo apt-get install -y python-pip
+sudo apt-get install -y xfonts-wqy ttf-wqy-microhei ttf-wqy-zenhei
+sudo apt-get install -y samba samba-common-bin
+sudo apt-get install -y samba
+sudo apt-get install -y smbclient
+sudo apt-get install -y subversion
+sudo apt-get install -y ntfs-3g
+sudo apt-get install -y audacity
+sudo apt-get install -y lirc
+sudo apt-get install -y python-dev
+sudo apt-get install -y python-pyaudio
+sudo apt-get install -y python-numpy
+sudo apt-get install -y python-qt4
+sudo apt-get install -y python-qwt5-qt4
+sudo apt-get install -y python-matplotlib
+sudo apt-get install -y python-scipy
+sudo apt-get install -y minicom
+sudo apt-get install -y python-smbus
+sudo pip install SoundAnalyse
+
+sudo apt-get install -y nmap
