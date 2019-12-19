30c317aa7427b02a92690d84866596c96d78ad05
robert.lo <robert.lo@amtran.com.tw>
2019-12-19 18:21:02 +0800

    [parameter] - To modify mac address for power off function

--- Python_Code/power_off_ir.sh
+++ Python_Code/power_off_ir.sh
@@ -1,5 +1,5 @@
 HiMediaMacAddr=B8:27:EB:E0:38:F9
-HiMediaMacIP=$(echo "$password\n" | sudo -S nmap -sP 192.168.0.* | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | grep -i "$HiMediaMacAddr" | sort)
+HiMediaMacIP=$(echo "$password\n" | sudo -S nmap -sP 192.168.11.* | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | grep -i "$HiMediaMacAddr" | sort)
 HiMediaMacIP=$(echo "$HiMediaMacIP" | sed 's/=>//g' | sed 's/B8:27:EB:E0:38:F9//g' | sed 's/^[ \t]*//g' | sed 's/[ \t]*$//g')
 echo "RaspX IP = $HiMediaMacIP"
 
